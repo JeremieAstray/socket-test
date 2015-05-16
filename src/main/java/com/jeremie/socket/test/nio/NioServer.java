4d8d6f8901da8fd29295a9eeec61d20031aa902e
叶冠鸿 <guanhong.ygh@alibaba-inc.com>
2015-05-16 09:25:22 +0800

    同步代码

--- src/main/java/com/jeremie/socket/test/nio/NioServer.java
+++ src/main/java/com/jeremie/socket/test/nio/NioServer.java
@@ -3,26 +3,40 @@ package com.jeremie.socket.test.nio;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
-import java.net.Socket;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.util.Set;
 
 /**
  * Created by Jeremie on 2015/5/15.
  */
 public class NioServer {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        ServerSocketChannel serverSocketChannel;
+        Selector selector = null;
         try {
-            ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+            serverSocketChannel = ServerSocketChannel.open();
             ServerSocket serverSocket = serverSocketChannel.socket();
             serverSocket.bind(new InetSocketAddress(8000));
             serverSocketChannel.configureBlocking(false);
-            Selector selector = Selector.open();
+            selector = Selector.open();
             serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
-
         } catch (IOException e) {
             e.printStackTrace();
         }
+        while (true) {
+            try {
+                if (selector == null)
+                    break;
+                selector.select();
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+            Set readyKeys = selector.selectedKeys();
+
+        }
+
     }
 }
